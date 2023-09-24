515eec89a758b970770f0218bc5fbebdc724c2c6
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-09-24 08:58:50 +0800

    install bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
+import './assets/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import { Popover } from 'bootstrap';
